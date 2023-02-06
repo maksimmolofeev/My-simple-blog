cc84b91ed6dd448f04df5c5485373ab251ff449f
maksimmolofeev <109696557+maksimmolofeev@users.noreply.github.com>
2023-02-06 11:44:27 +0300

    config decomposition. Configuration Options

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,25 @@
+import { BuildOptions } from "./types/config";
+import webpack from 'webpack'
+import { buildLoaders } from "./buildLoaders";
+import { buildPlugins } from "./buildPlugins";
+import { buildResolvers } from "./buildResolvers";
+
+export function buildWebpackConfig (options: BuildOptions): webpack.Configuration {
+
+    const {mode, paths} = options
+
+    return {
+        mode: mode,
+        entry: paths.entry,
+        output: {
+          filename: '[name].[contenthash].js',
+          path: paths.build,
+          clean: true
+        },
+        plugins: buildPlugins(options),
+        module: {
+          rules: buildLoaders(),
+        },
+        resolve: buildResolvers(),
+    }
+}
