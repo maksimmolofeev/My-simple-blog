ba3c5e84d029d40c7fd0dd514a31b0a91346bbbd
maksimmolofeev <109696557+maksimmolofeev@users.noreply.github.com>
2023-02-08 20:39:01 +0300

    Connected React. Set up CSS and css modules.

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,5 +1,6 @@
-import webpack from 'webpack'
+import webpack, { Chunk } from 'webpack'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import { BuildOptions } from "./types/config";
 
 export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
@@ -9,5 +10,9 @@ export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstan
         new HtmlWebpackPlugin({
             template: paths.html
         }),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css'
+        })
     ]
 }
