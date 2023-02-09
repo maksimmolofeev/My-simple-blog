9ce255098c560282e26a69f7ee85d4ae65d48e46
maksimmolofeev <109696557+maksimmolofeev@users.noreply.github.com>
2023-02-09 14:24:26 +0300

    Routing Code splitting Lazy Suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { Suspense } from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import { Counter } from "./components/Counter";
 import './index.scss'
+import AboutPage from "./page/AboutPage/AboutPage";
+import { AboutPageAsync } from "./page/AboutPage/AboutPage.async";
+import MainPage from "./page/MainPage/MainPage";
+import { MainPageAsync } from "./page/MainPage/MainPage.async";
 
 const App = () => {
     return (
         <div className="app">
-            sdazfsvasff
-            <Counter />
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/'} element={<MainPageAsync />} />
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { Counter } from "./components/Counter";
 
 
 render (
-    <App />,
+    <BrowserRouter>
+        <App />
+    </BrowserRouter> ,
     document.getElementById('root')
 )
